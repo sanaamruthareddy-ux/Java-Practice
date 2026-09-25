da8a87ce8717153d00322a4d9f985459f06c48e5
sanaamruthareddy-ux <sanaamruthareddy@gmail.com>
2026-09-25 11:38:19 +0530

    Add files via upload

---
+++ bytecode.java
@@ -0,0 +1,21 @@
+package Javaprogram;
+
+public class bytecode {
+
+	public static void main(String[] args) {
+		byte b = 127;
+		short s = -32765;
+		short s2 = b;
+		byte b2 =(byte)s;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(s2);
+		System.out.println(b2);
+	}
+}
+		
+		
+
+	
+
+
